423fd40633cd153972f3ed6f312432c4fabbe37c
dgarmash <dgarmash@synergy.ru>
2022-11-07 21:54:25 +0300

    Add new game made with sfml

---
+++ csharp/sfml_game/Game/Game.cs
@@ -0,0 +1,31 @@
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using SFML.Audio;
+
+namespace Program
+{
+    public class Game : GameLoop {
+        public const uint DEFEAULT_WINDOW_HEIGHT = 480;
+        public const uint DEFEAULT_WINDOW_WIDTH = 640;
+        public const string WINDOW_TITLE = "SfmlGame";
+
+        public Game() : base(DEFEAULT_WINDOW_WIDTH, DEFEAULT_WINDOW_HEIGHT, WINDOW_TITLE, Color.Yellow) {
+            
+        }
+
+        public override void LoadContent() {
+
+        }
+        public override void Initialize() {
+
+        }
+        public override void Update(GameTime gameTime) {
+
+        }
+        public override void Draw(GameTime gameTime)
+        {
+
+        }
+    }
+}
